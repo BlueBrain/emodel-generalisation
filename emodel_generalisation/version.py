a16ac10354e0672cae5a0c85ba205f0c7920d7b8
arnaudon <alexis.arnaudon@epfl.ch>
2023-11-30 12:52:10 +0100

    Release 0.2.2

--- emodel_generalisation/version.py
+++ emodel_generalisation/version.py
@@ -19,4 +19,4 @@
 # or send a letter to Creative Commons, 171
 # Second Street, Suite 300, San Francisco, California, 94105, USA.
 
-VERSION = "0.1.1"  # pragma: no cover
+VERSION = "0.2.0"  # pragma: no cover
