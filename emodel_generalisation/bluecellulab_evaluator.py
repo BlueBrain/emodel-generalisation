287d310407ce573e3e0f5bc80242337ec3f8251e
arnaudon <alexis.arnaudon@epfl.ch>
2024-05-07 12:13:12 +0200

    fix

--- emodel_generalisation/bluecellulab_evaluator.py
+++ emodel_generalisation/bluecellulab_evaluator.py
@@ -7,8 +7,8 @@ from multiprocessing.context import TimeoutError  # pylint: disable=redefined-bu
 from pathlib import Path
 
 import bluecellulab
-from bluecellulab.simulation.neuron_globals import NeuronGlobals
 import efel
+from bluecellulab.simulation.neuron_globals import NeuronGlobals
 from bluepyparallel.evaluator import evaluate
 from bluepyparallel.parallel import NestedPool
 
